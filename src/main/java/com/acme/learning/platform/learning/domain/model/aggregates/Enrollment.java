a68c9d88b90d3d1c222d7c8137929228977a70e7
Angel Velasquez <avelasquezn@gmail.com>
2023-10-17 08:04:30 -0500

    feat(enrollments): added enrollment aggregate.

---
+++ src/main/java/com/acme/learning/platform/learning/domain/model/aggregates/Enrollment.java
@@ -0,0 +1,25 @@
+package com.acme.learning.platform.learning.domain.model.aggregates;
+
+import com.acme.learning.platform.learning.domain.model.valueobjects.AcmeStudentRecordId;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.domain.AbstractAggregateRoot;
+
+@Entity
+public class Enrollment extends AbstractAggregateRoot<Enrollment> {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Embedded
+    private AcmeStudentRecordId acmeStudentRecordId;
+
+    @Getter
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+
+}
---
+++ src/main/java/com/acme/learning/platform/learning/domain/model/valueobjects/EnrollmentStatus.java
@@ -0,0 +1,8 @@
+package com.acme.learning.platform.learning.domain.model.valueobjects;
+
+public enum EnrollmentStatus {
+    REQUESTED,
+    CONFIRMED,
+    REJECTED,
+    CANCELLED
+}
