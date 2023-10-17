62bfd110f9eb463a4575da41809d9f5675137ec6
Angel Velasquez <avelasquezn@gmail.com>
2023-10-17 09:12:28 -0500

    feat(enrollments): added business logic related to progress record.

--- src/main/java/com/acme/learning/platform/learning/domain/model/aggregates/Enrollment.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/aggregates/Enrollment.java
@@ -1,6 +1,8 @@
 package com.acme.learning.platform.learning.domain.model.aggregates;
 
 import com.acme.learning.platform.learning.domain.model.valueobjects.AcmeStudentRecordId;
+import com.acme.learning.platform.learning.domain.model.valueobjects.EnrollmentStatus;
+import com.acme.learning.platform.learning.domain.model.valueobjects.ProgressRecord;
 import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.domain.AbstractAggregateRoot;
@@ -25,5 +27,48 @@ public class Enrollment extends AbstractAggregateRoot<Enrollment> {
     @JoinColumn(name = "course_id")
     private Course course;
 
+    /***
+     * The progress record for this enrollment
+     */
+    @Embedded
+    private ProgressRecord progressRecord;
+
+    private EnrollmentStatus status;
+
+    public Enrollment() {
+
+    }
+
+    public Enrollment(AcmeStudentRecordId studentRecordId, Course course) {
+        this.acmeStudentRecordId = studentRecordId;
+        this.course = course;
+        this.status = EnrollmentStatus.REQUESTED;
+        this.progressRecord = new ProgressRecord();
+    }
+
+    public void confirm() {
+        this.status = EnrollmentStatus.CONFIRMED;
+        this.progressRecord.initializeProgressRecord(this, course.getLearningPath());
+    }
+
+    public void reject() {
+        this.status = EnrollmentStatus.REJECTED;
+    }
+
+    public void cancel() {
+        this.status = EnrollmentStatus.CANCELLED;
+    }
+
+    public long calculateDaysElapsed() {
+        return this.progressRecord.calculateDaysElapsedForEnrollment(this);
+    }
+
+    public boolean isConfirmed() {
+        return this.status == EnrollmentStatus.CONFIRMED;
+    }
+
+    public boolean isRejected() {
+        return this.status == EnrollmentStatus.REJECTED;
+    }
 
 }
