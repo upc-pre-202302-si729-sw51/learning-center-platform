a20c7c3a01eaadd37d0c9881e48febf5343d43cf
Angel Velasquez <avelasquezn@gmail.com>
2023-10-17 08:30:48 -0500

    feat(enrollments): added progress record relationship with progress record items.

--- src/main/java/com/acme/learning/platform/learning/domain/model/valueobjects/ProgressRecord.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/valueobjects/ProgressRecord.java
@@ -1,5 +1,21 @@
 package com.acme.learning.platform.learning.domain.model.valueobjects;
 
+import com.acme.learning.platform.learning.domain.model.entities.ProgressRecordItem;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
 public class ProgressRecord {
+    @OneToMany(mappedBy = "enrollment")
+    private List<ProgressRecordItem> progressRecordItems;
+
+
+    public ProgressRecord() {
+        progressRecordItems = new ArrayList<>();
+    }
+
 
 }
