6aebd5f0b4f8d313291b490364fb5fb31e339bae
Angel Velasquez <avelasquezn@gmail.com>
2023-10-19 08:39:25 -0500

    feat(enrollments): configured enrollment query service implementation as a service.

--- src/main/java/com/acme/learning/platform/learning/application/internal/queryservices/EnrollmentQueryServiceImpl.java
+++ src/main/java/com/acme/learning/platform/learning/application/internal/queryservices/EnrollmentQueryServiceImpl.java
@@ -4,9 +4,11 @@ import com.acme.learning.platform.learning.domain.model.aggregates.Enrollment;
 import com.acme.learning.platform.learning.domain.model.queries.GetStudentEnrollmentsQuery;
 import com.acme.learning.platform.learning.domain.services.EnrollmentQueryService;
 import com.acme.learning.platform.learning.infrastructure.repositories.EnrollmentRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EnrollmentQueryServiceImpl implements EnrollmentQueryService {
     private final EnrollmentRepository enrollmentRepository;
 
