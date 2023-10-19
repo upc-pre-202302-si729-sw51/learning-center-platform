d318b3c76f5f65d55f871cfed046666b340ffcfc
Angel Velasquez <avelasquezn@gmail.com>
2023-10-19 07:39:36 -0500

    chore: configured snake case database physical naming strategy.

--- src/main/java/com/acme/learning/platform/shared/infrastructure/persistence/jpa/SnakeCasePhysicalNamingStrategy.java
+++ src/main/java/com/acme/learning/platform/shared/infrastructure/persistence/jpa/SnakeCasePhysicalNamingStrategy.java
@@ -1,4 +1,4 @@
-package com.acme.learning.platform.shared.infrastructure.persistance.jpa;
+package com.acme.learning.platform.shared.infrastructure.persistence.jpa;
 
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
