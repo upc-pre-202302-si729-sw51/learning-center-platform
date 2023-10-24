5a2fabef5fa2184346be399e67ac1517483da163
jmayta1984 <jmayta@pucp.edu.pe>
2023-10-24 08:28:39 -0500

    feat(profiles): implemented resource and transform components for interface layer.

---
+++ src/main/java/com/acme/learning/platform/profiles/interfaces/rest/resources/CreateProfileResource.java
@@ -0,0 +1,13 @@
+package com.acme.learning.platform.profiles.interfaces.rest.resources;
+
+public record CreateProfileResource(
+        String firstName,
+        String lastName,
+        String email,
+        String street,
+        String number,
+        String city,
+        String zipCode,
+        String country
+) {
+}
---
+++ src/main/java/com/acme/learning/platform/profiles/interfaces/rest/resources/ProfileResource.java
@@ -0,0 +1,9 @@
+package com.acme.learning.platform.profiles.interfaces.rest.resources;
+
+public record ProfileResource(
+        Long id,
+        String fullName,
+        String email,
+        String streetAddress
+) {
+}
