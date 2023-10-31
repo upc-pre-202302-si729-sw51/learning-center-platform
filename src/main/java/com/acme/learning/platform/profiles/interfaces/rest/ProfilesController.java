a4d8fc08087e9823f02afcdc905b5f398e9a48ec
Angel Velasquez <avelasquezn@gmail.com>
2023-10-31 08:00:51 -0500

    feat(profiles): added get all profiles endpoint.

--- src/main/java/com/acme/learning/platform/profiles/interfaces/rest/ProfilesController.java
+++ src/main/java/com/acme/learning/platform/profiles/interfaces/rest/ProfilesController.java
@@ -1,5 +1,6 @@
 package com.acme.learning.platform.profiles.interfaces.rest;
 
+import com.acme.learning.platform.profiles.domain.model.queries.GetAllProfilesQuery;
 import com.acme.learning.platform.profiles.domain.model.queries.GetProfileByIdQuery;
 import com.acme.learning.platform.profiles.domain.services.ProfileCommandService;
 import com.acme.learning.platform.profiles.domain.services.ProfileQueryService;
@@ -13,6 +14,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * ProfilesController
+ * <p>
+ *     This class is the entry point for all the REST endpoints related to the Profile entity.
+ * </p>
+ */
 @RestController
 @RequestMapping(value = "/api/v1/profiles", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Profiles", description = "Profile Management Endpoints")
@@ -25,6 +35,11 @@ public class ProfilesController {
         this.profileCommandService = profileCommandService;
     }
 
+    /**
+     * Creates a new Profile
+     * @param resource the resource containing the data to create the Profile
+     * @return the created Profile
+     */
     @PostMapping
     public ResponseEntity<ProfileResource> createProfile(@RequestBody CreateProfileResource resource) {
         var createProfileCommand = CreateProfileCommandFromResourceAssembler.toCommandFromResource(resource);
@@ -43,6 +58,11 @@ public class ProfilesController {
         return new ResponseEntity<>(profileResource, HttpStatus.CREATED);
     }
 
+    /**
+     * Gets a Profile by its id
+     * @param profileId the id of the Profile to get
+     * @return the Profile resource associated to given Profile id
+     */
     @GetMapping("/{profileId}")
     public ResponseEntity<ProfileResource> getProfileById(@PathVariable Long profileId) {
         var getProfileByIdQuery = new GetProfileByIdQuery(profileId);
@@ -50,5 +70,17 @@ public class ProfilesController {
         if (profile.isEmpty()) return ResponseEntity.badRequest().build();
         var profileResource = ProfileResourceFromEntityAssembler.toResourceFromEntity(profile.get());
         return ResponseEntity.ok(profileResource);
+    }
+
+    /**
+     * Gets all the Profiles
+     * @return a list of all the Profile resources currently stored
+     */
+    @GetMapping
+    public ResponseEntity<List<ProfileResource>> getAllProfiles() {
+        var getAllProfilesQuery = new GetAllProfilesQuery();
+        var profiles = profileQueryService.handle(getAllProfilesQuery);
+        var profileResources = profiles.stream().map(ProfileResourceFromEntityAssembler::toResourceFromEntity).collect(Collectors.toList());
+        return ResponseEntity.ok(profileResources);
     }
 }
