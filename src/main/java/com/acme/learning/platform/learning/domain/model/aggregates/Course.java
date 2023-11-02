d5117e7ab61b91a8dea59faf5eff980192c039c7
Angel Velasquez <avelasquezn@gmail.com>
2023-11-02 09:59:14 -0500

    feat(courses): added learning path initialization in constructor.

--- src/main/java/com/acme/learning/platform/learning/domain/model/aggregates/Course.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/aggregates/Course.java
@@ -5,33 +5,47 @@ import com.acme.learning.platform.shared.domain.model.entities.AuditableModel;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.util.Strings;
 
+/**
+ * Represents a course.
+ * The course is an aggregate root
+ */
+@Getter
 @Entity
 public class Course extends AuditableModel {
-    @Getter
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Getter
-    @Setter
     private String title;
 
-    @Getter
-    @Setter
     private String description;
 
+    /**
+     * The learning path for this course
+     */
     @Embedded
-    @Getter
-    private LearningPath learningPath;
+    private final LearningPath learningPath;
 
     public Course() {
+        this.title = Strings.EMPTY;
+        this.description = Strings.EMPTY;
+        this.learningPath = new LearningPath();
     }
     public Course(String title, String description) {
+        this();
         this.title = title;
         this.description = description;
     }
 
+    /**
+     * Updates the information for this course
+     * @param title the new title
+     * @param description the new description
+     * @return the updated course
+     */
     public Course updateInformation(String title, String description) {
         this.title = title;
         this.description = description;
