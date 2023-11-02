314ff2c647c339d9f14ab2121407f29f74a1c02d
Angel Velasquez <avelasquezn@gmail.com>
2023-11-02 07:41:32 -0500

    feat(students): added student enrollments endpoint.

---
+++ src/main/java/com/acme/learning/platform/learning/interfaces/rest/StudentEnrollmentsController.java
@@ -0,0 +1,55 @@
+package com.acme.learning.platform.learning.interfaces.rest;
+
+import com.acme.learning.platform.learning.domain.model.queries.GetStudentEnrollmentsQuery;
+import com.acme.learning.platform.learning.domain.model.valueobjects.AcmeStudentRecordId;
+import com.acme.learning.platform.learning.domain.services.EnrollmentQueryService;
+import com.acme.learning.platform.learning.interfaces.rest.resources.EnrollmentResource;
+import com.acme.learning.platform.learning.interfaces.rest.transform.EnrollmentResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+/**
+ * REST controller for managing student enrollments.
+ * <p>
+ * This controller exposes the following endpoints:
+ * <ul>
+ *     <li>GET /api/v1/students/{studentRecordId}/enrollments</li>
+ * </ul>
+ * </p>
+ */
+@RestController
+@RequestMapping(value = "/api/v1/students/{studentRecordId}/enrollments", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Students")
+public class StudentEnrollmentsController {
+    private final EnrollmentQueryService enrollmentQueryService;
+
+    public StudentEnrollmentsController(EnrollmentQueryService enrollmentQueryService) {
+        this.enrollmentQueryService = enrollmentQueryService;
+    }
+
+    /**
+     * {@code GET /api/v1/students/{studentRecordId}/enrollments} : Get all the enrollments for a student.
+     *
+     * @param studentRecordId the student record id.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of enrollment resources in body.
+     * @see EnrollmentResource
+     */
+    @GetMapping
+    public ResponseEntity<List<EnrollmentResource>> getStudentEnrollments(@PathVariable String studentRecordId) {
+        var acmeStudentRecordId = new AcmeStudentRecordId(studentRecordId);
+        var getStudentEnrollmentsQuery = new GetStudentEnrollmentsQuery(acmeStudentRecordId);
+        var enrollments = enrollmentQueryService.handle(getStudentEnrollmentsQuery);
+        var enrollmentResources = enrollments.stream().map(EnrollmentResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(enrollmentResources);
+    }
+
+
+}
