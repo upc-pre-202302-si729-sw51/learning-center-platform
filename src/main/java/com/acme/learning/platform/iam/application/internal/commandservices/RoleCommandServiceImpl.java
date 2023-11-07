244da0437ab63b1a265f2fe69da96ad10096117e
Angel Velasquez <avelasquezn@gmail.com>
2023-11-07 08:33:07 -0500

    feat(roles): added role command service implementation.

---
+++ src/main/java/com/acme/learning/platform/iam/application/internal/commandservices/RoleCommandServiceImpl.java
@@ -0,0 +1,33 @@
+package com.acme.learning.platform.iam.application.internal.commandservices;
+
+import com.acme.learning.platform.iam.domain.model.commands.SeedRolesCommand;
+import com.acme.learning.platform.iam.domain.model.entities.Role;
+import com.acme.learning.platform.iam.domain.model.valueobjects.Roles;
+import com.acme.learning.platform.iam.domain.services.RoleCommandService;
+import com.acme.learning.platform.iam.infrastructure.persistence.jpa.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+/**
+ * Implementation of {@link RoleCommandService}
+ */
+@Service
+public class RoleCommandServiceImpl implements RoleCommandService {
+    private final RoleRepository roleRepository;
+
+    public RoleCommandServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handle(SeedRolesCommand command) {
+        Arrays.stream(Roles.values()).forEach(role -> {
+            if (!roleRepository.existsByName(role))
+                roleRepository.save(new Role(Roles.valueOf(role.name())));
+        });
+    }
+}
