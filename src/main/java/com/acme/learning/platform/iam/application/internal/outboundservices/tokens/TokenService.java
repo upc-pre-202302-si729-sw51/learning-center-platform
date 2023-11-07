7f65e54a058c5831fbb54a70f651a036b2003ccc
Angel Velasquez <avelasquezn@gmail.com>
2023-11-07 09:26:39 -0500

    docs: added documentation to token service interface

--- src/main/java/com/acme/learning/platform/iam/application/internal/outboundservices/tokens/TokenService.java
+++ src/main/java/com/acme/learning/platform/iam/application/internal/outboundservices/tokens/TokenService.java
@@ -2,11 +2,32 @@ package com.acme.learning.platform.iam.application.internal.outboundservices.tok
 
 import org.springframework.security.core.Authentication;
 
+/**
+ * Token service interface.
+ * <p>
+ *     This interface is used to generate and validate a token.
+ * </p>
+ */
 public interface TokenService {
+    /**
+     * Generates a token.
+     * @param authentication the authentication object.
+     * @return the generated token.
+     */
     String generateToken(Authentication authentication);
 
+    /**
+     * Gets the username from a token.
+     * @param token the token.
+     * @return the username.
+     */
     String getUsernameFromToken(String token);
 
+    /**
+     * Validates a token.
+     * @param token the token.
+     * @return true if the token is valid, false otherwise.
+     */
     boolean validateToken(String token);
 }
 
