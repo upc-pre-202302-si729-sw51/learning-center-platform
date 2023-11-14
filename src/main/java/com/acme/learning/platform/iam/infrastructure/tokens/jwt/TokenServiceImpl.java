4a212b1a9e572f8626276453c62fa47243149a16
Angel Velasquez <avelasquezn@gmail.com>
2023-11-14 08:10:16 -0500

    feat(jwt): partial implementation of token service.

--- src/main/java/com/acme/learning/platform/iam/infrastructure/tokens/jwt/TokenServiceImpl.java
+++ src/main/java/com/acme/learning/platform/iam/infrastructure/tokens/jwt/TokenServiceImpl.java
@@ -1,11 +1,20 @@
 package com.acme.learning.platform.iam.infrastructure.tokens.jwt;
 
 import com.acme.learning.platform.iam.application.internal.outboundservices.tokens.TokenService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class TokenServiceImpl implements TokenService {
@@ -18,15 +27,45 @@ public class TokenServiceImpl implements TokenService {
 
     private static final int TOKEN_BEGIN_INDEX = 7;
 
-    @Value("${jwt.secret}")
+    @Value("${authorization.jwt.secret}")
     private String secret;
 
-    @Value("${jwt.expiration.days}")
+    @Value("${authorization.jwt.expiration.days}")
     private int expirationDays;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    private String buildTokenWithDefaultParameters(String username) {
+        var issuedAt = new Date();
+        var expiration = DateUtils.addDays(issuedAt, expirationDays);
+        var key = getSigningKey();
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(issuedAt)
+                .expiration(expiration)
+                .signWith(key)
+                .compact();
+    }
+
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
     @Override
     public String generateToken(Authentication authentication) {
-        return null;
+        return buildTokenWithDefaultParameters(authentication.getName());
+    }
+
+    public String generateToken(String username) {
+        return buildTokenWithDefaultParameters(username);
     }
 
     @Override
