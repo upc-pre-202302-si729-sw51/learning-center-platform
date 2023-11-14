c71320149efbc9a54a479adcc35af6ec889140ed
Angel Velasquez <avelasquezn@gmail.com>
2023-11-14 07:50:29 -0500

    chore: added configuration parameters.

--- src/main/java/com/acme/learning/platform/iam/infrastructure/tokens/jwt/TokenServiceImpl.java
+++ src/main/java/com/acme/learning/platform/iam/infrastructure/tokens/jwt/TokenServiceImpl.java
@@ -1,11 +1,29 @@
 package com.acme.learning.platform.iam.infrastructure.tokens.jwt;
 
 import com.acme.learning.platform.iam.application.internal.outboundservices.tokens.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TokenServiceImpl implements TokenService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(TokenServiceImpl.class);
+
+    private static final String AUTHORIZATION_PARAMETER_NAME = "Authorization";
+
+    private static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    private static final int TOKEN_BEGIN_INDEX = 7;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration.days}")
+    private int expirationDays;
+
     @Override
     public String generateToken(Authentication authentication) {
         return null;
