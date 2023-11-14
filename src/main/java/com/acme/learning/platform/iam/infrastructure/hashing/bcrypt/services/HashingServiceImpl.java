cc148e1bff3720eace165cf0474a9426280340db
Angel Velasquez <avelasquezn@gmail.com>
2023-11-14 07:40:23 -0500

    docs: added documentation comments to hashing service implementation.

--- src/main/java/com/acme/learning/platform/iam/infrastructure/hashing/bcrypt/services/HashingServiceImpl.java
+++ src/main/java/com/acme/learning/platform/iam/infrastructure/hashing/bcrypt/services/HashingServiceImpl.java
@@ -1,6 +1,5 @@
 package com.acme.learning.platform.iam.infrastructure.hashing.bcrypt.services;
 
-import com.acme.learning.platform.iam.application.internal.outboundservices.hashing.HashingService;
 import com.acme.learning.platform.iam.infrastructure.hashing.bcrypt.BCryptHashingService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,13 +21,23 @@ public class HashingServiceImpl implements BCryptHashingService {
     }
 
     /**
-     * @InheritDoc
+     * Hashes a password.
+     * @param rawPassword the password to hash.
+     * It must be a {@link CharSequence} object.
+     * return the hashed password.
      */
     @Override
     public String encode(CharSequence rawPassword) {
         return passwordEncoder.encode(rawPassword);
     }
 
+    /**
+     * Matches a raw password with an encoded password.
+     * @param rawPassword the raw password to match.
+     * It must be a {@link CharSequence} object.
+     * @param encodedPassword the encoded password to match.
+     * @return true if the raw password matches the encoded password, false otherwise.
+     */
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
         return passwordEncoder.matches(rawPassword, encodedPassword);
