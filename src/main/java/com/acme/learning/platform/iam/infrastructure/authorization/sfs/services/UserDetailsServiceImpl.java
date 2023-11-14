3339efd90c871760f61397233c937ff63ccbae64
Angel Velasquez <avelasquezn@gmail.com>
2023-11-14 09:18:37 -0500

    feat(authorization): added authorization request pipeline classes.

---
+++ src/main/java/com/acme/learning/platform/iam/infrastructure/authorization/sfs/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.acme.learning.platform.iam.infrastructure.authorization.sfs.services;
+
+import com.acme.learning.platform.iam.infrastructure.authorization.sfs.model.UserDetailsImpl;
+import com.acme.learning.platform.iam.infrastructure.persistence.jpa.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("defaultUserDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
