8082f0b94fd46f6762861b9ffb27025254f134bd
Angel Velasquez <avelasquezn@gmail.com>
2023-11-14 09:43:25 -0500

    docs: added security configuration to open api documentation.

--- src/main/java/com/acme/learning/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/learning/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package com.acme.learning.platform.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +14,9 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfiguration {
     @Bean
     public OpenAPI learningPlatformOpenApi() {
-        return new OpenAPI()
+        final String securitySchemaName = "bearerAuth";
+        var openApi = new OpenAPI();
+        openApi
                 .info(new Info()
                         .title("ACME Learning Platform API")
                         .description("ACME Learning Platform application REST API documentation.")
@@ -21,5 +26,17 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("ACME Learning Platform wiki Documentation")
                         .url("https://acme-learning-platform.wiki.github.io/docs"));
+
+        openApi
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemaName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemaName,
+                                new SecurityScheme()
+                                        .name(securitySchemaName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+        return openApi;
     }
 }
