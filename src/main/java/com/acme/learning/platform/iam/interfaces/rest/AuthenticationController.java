5021d1f861b332b6e5a1de04eb37f5dd9cbd62c2
Angel Velasquez <avelasquezn@gmail.com>
2023-11-14 11:07:00 -0500

    feat(iam): implemented interface layer for authentication.

--- src/main/java/com/acme/learning/platform/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/acme/learning/platform/iam/interfaces/rest/AuthenticationController.java
@@ -1,8 +1,20 @@
 package com.acme.learning.platform.iam.interfaces.rest;
 
 import com.acme.learning.platform.iam.domain.services.UserCommandService;
+import com.acme.learning.platform.iam.interfaces.rest.resources.AuthenticatedUserResource;
+import com.acme.learning.platform.iam.interfaces.rest.resources.SignInResource;
+import com.acme.learning.platform.iam.interfaces.rest.resources.SignUpResource;
+import com.acme.learning.platform.iam.interfaces.rest.resources.UserResource;
+import com.acme.learning.platform.iam.interfaces.rest.transform.AuthenticatedUserResourceFromEntityAssembler;
+import com.acme.learning.platform.iam.interfaces.rest.transform.SignInCommandFromResourceAssembler;
+import com.acme.learning.platform.iam.interfaces.rest.transform.SignUpCommandFromResourceAssembler;
+import com.acme.learning.platform.iam.interfaces.rest.transform.UserResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +28,26 @@ public class AuthenticationController {
         this.userCommandService = userCommandService;
     }
 
+    @PostMapping("/sign-in")
+    public ResponseEntity<AuthenticatedUserResource> signIn(@RequestBody SignInResource signInResource) {
+        var signInCommand = SignInCommandFromResourceAssembler.toCommandFromResource(signInResource);
+        var authenticatedUser = userCommandService.handle(signInCommand);
+        if (authenticatedUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var authenticatedUserResource = AuthenticatedUserResourceFromEntityAssembler.toResourceFromEntity(authenticatedUser.get().getLeft(), authenticatedUser.get().getRight());
+        return ResponseEntity.ok(authenticatedUserResource);
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<UserResource> signUp(@RequestBody SignUpResource signUpResource) {
+        var signUpCommand = SignUpCommandFromResourceAssembler.toCommandFromResource(signUpResource);
+        var user = userCommandService.handle(signUpCommand);
+        if (user.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(user.get());
+        return new ResponseEntity<>(userResource, HttpStatus.CREATED);
+    }
 
 }
---
+++ src/main/java/com/acme/learning/platform/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -0,0 +1,12 @@
+package com.acme.learning.platform.iam.interfaces.rest.transform;
+
+import com.acme.learning.platform.iam.domain.model.aggregates.User;
+import com.acme.learning.platform.iam.domain.model.entities.Role;
+import com.acme.learning.platform.iam.interfaces.rest.resources.UserResource;
+
+public class UserResourceFromEntityAssembler {
+    public static UserResource toResourceFromEntity(User user) {
+        var roles = user.getRoles().stream().map(Role::getStringName).toList();
+        return new UserResource(user.getId(), user.getUsername(), roles);
+    }
+}
